46a6a353934cc1157ed752ce93633dac3a97340b
nshea3 <nshea@email.arizona.edu>
2018-11-14 19:30:11 -0700

    Modified mdp code, now getting time and utility function for policy iteration

--- mdp.py
+++ mdp.py
@@ -9,6 +9,7 @@ and policy_iteration algorithms."""
 from utils import argmax, vector_add, orientations, turn_right, turn_left
 
 import random
+from timeit import default_timer as timer
 import numpy as np
 from collections import defaultdict
 
@@ -203,15 +204,19 @@ def value_iteration(mdp, epsilon=0.001):
 
     U1 = {s: 0 for s in mdp.states}
     R, T, gamma = mdp.R, mdp.T, mdp.gamma
+    iter_time = []
     while True:
+        start_t = timer()
         U = U1.copy()
         delta = 0
         for s in mdp.states:
             U1[s] = R(s) + gamma * max(sum(p*U[s1] for (p, s1) in T(s, a))
                                                    for a in mdp.actions(s))
             delta = max(delta, abs(U1[s] - U[s]))
+        end_t = timer()
+        iter_time.append(end_t - start_t)
         if delta <= epsilon*(1 - gamma)/gamma:
-            return U
+            return U, iter_time
 
 
 def best_policy(mdp, U):
@@ -237,7 +242,9 @@ def policy_iteration(mdp):
 
     U = {s: 0 for s in mdp.states}
     pi = {s: random.choice(mdp.actions(s)) for s in mdp.states}
+    iter_time = []
     while True:
+        start_t = timer()
         U = policy_evaluation(pi, U, mdp)
         unchanged = True
         for s in mdp.states:
@@ -245,8 +252,10 @@ def policy_iteration(mdp):
             if a != pi[s]:
                 pi[s] = a
                 unchanged = False
+        end_t = timer()
+        iter_time.append(end_t - start_t)
         if unchanged:
-            return pi
+            return pi, U, iter_time
 
 
 def policy_evaluation(pi, U, mdp, k=1):
